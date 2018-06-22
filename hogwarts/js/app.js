1216ee23184992a28117a64a51c53351f394ecb0
Amanda Molde <amanda.molde@gmail.com>
2018-06-21 18:58:07 -0600

    Commit 1 - I added my first element and it was fun

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -3,3 +3,14 @@
 // } else {
 //   console.log('I did it! I linked jQuery and this js file!')
 // };
+
+
+ $(() => {
+	// put jQuery in here
+	const $container = $('#container');
+	// console.log($container);
+	const $h1 = $('<h1></h1>');
+	// console.log($h1);
+	$container.append("<h1>Hogwarts</h1>"); // <-- this works
+	// $h1.text("Hogwarts"); // <-- this doesn't work
+});
