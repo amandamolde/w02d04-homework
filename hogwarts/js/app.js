6a191265306c428b174b5aeae415ac1723ad0957
Amanda Molde <amanda.molde@gmail.com>
2018-06-21 19:31:49 -0600

    Commit 2 - I added more elements. My power grows with my dedication and hard work.

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -8,9 +8,24 @@
  $(() => {
 	// put jQuery in here
 	const $container = $('#container');
-	// console.log($container);
+
 	const $h1 = $('<h1></h1>');
-	// console.log($h1);
-	$container.append("<h1>Hogwarts</h1>"); // <-- this works
-	// $h1.text("Hogwarts"); // <-- this doesn't work
+	$h1.text('Hogwarts');
+	$container.append($h1);
+
+	const $h2 = $('<h2></h2>')
+	$h2.text('Amanda');
+	$container.append($h2);
+
+	const $h3 = $('<h3></h3>');
+	$h3.text('Gryffindor');
+	$container.append($h3);
+
+	const $h4 = $('<h4></h4>');
+	$h4.text('Leo');
+	$h4.addClass('owl');
+	$container.append($h4);
+
+	$container.append('<h4>Hornbeam Wand with Dragon Heartstring Core</h4>');
+
 });
